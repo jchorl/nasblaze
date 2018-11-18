5957fce4aa1e0dadf513bdb9fc5f654e32ba8706
Josh Chorlton <jchorlton@gmail.com>
2018-11-17 22:28:45 -0800

    fixes

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/golang/glog"
 	watchdog "github.com/jchorl/watchdog/client"
 	watchdog_types "github.com/jchorl/watchdog/types"
+	"github.com/ncw/rclone/fs"
 
 	"github.com/jchorl/nasblaze/drives"
 	"github.com/jchorl/nasblaze/rclone"
 )
 
 const (
-	driveSize  = "931.3G"
-	mountpoint = "/sync"
-	bucketName = "backup-c3bac1e7-d888-4940-8778-f03adcddfe45"
-	configPath = "/home/j/nasblaze/rclone.conf"
+	driveSize    = "931.3G"
+	mountpoint   = "/sync"
+	bucketName   = "backup-c3bac1e7-d888-4940-8778-f03adcddfe45"
+	configPath   = "/home/j/nasblaze/rclone.conf"
+	otherLogFile = "/home/j/logs/nasblaze.OTHER"
 )
 
 var (
@@ -29,10 +33,18 @@ func main() {
 	flag.Var(&filters, "exclude", "Exclude flags passed directly to rclone")
 	flag.Parse()
 
+	// set rclone log level
+	fs.Config.LogLevel = fs.LogLevelInfo
+	logFile, err := initGolangLog()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
 	glog.Info("Starting")
 
 	// mount the drive, if necessary
-	err := drives.MountDriveBySize(driveSize, mountpoint)
+	err = drives.MountDriveBySize(driveSize, mountpoint)
 	if err != nil {
 		glog.Fatalf("Error mounting drive: %s", err)
 	}
@@ -45,6 +57,20 @@ func main() {
 	wdClient := watchdog.Client{"https://watchdog.joshchorlton.com"}
 	wdClient.Ping("nasblaze", watchdog_types.Watch_WEEKLY)
 	glog.Info("Complete")
+}
+
+// rclone uses standard golang log package
+// this will ensure that those logs get written to file
+func initGolangLog() (*os.File, error) {
+	f, err := os.OpenFile(otherLogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		glog.Errorf("Error opening other log file: %v", err)
+		return nil, err
+	}
+
+	log.SetOutput(f)
+	log.Println("Other logging initted")
+	return f, nil
 }
 
 type arrayFlags []string
